cb943c853b371fe15e3d4f91915598807b394ce0
Driste <6456773+Driste@users.noreply.github.com>
2023-04-25 10:56:28 -0600

    Add support for cyclonedx print, validate, and export

--- cmd/export.go
+++ cmd/export.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -24,7 +26,8 @@ func NewExportCmd(service DDExportService, timeout time.Duration, engagement def
 		Short:   "export raw scan report to Defect Dojo",
 		Aliases: []string{"dd"},
 		Args:    cobra.ExactArgs(1),
-		RunE: func(_ *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fullBom, _ := cmd.Flags().GetBool("full-bom")
 			// Open the file
 			log.Infof("Opening file: %s", args[0])
 			f, err := os.Open(args[0])
@@ -44,6 +47,8 @@ func NewExportCmd(service DDExportService, timeout time.Duration, engagement def
 
 			var ddScanType defectdojo.ScanType
 			switch rType {
+			case artifact.Cyclonedx:
+				ddScanType = defectdojo.CycloneDX
 			case artifact.Grype:
 				ddScanType = defectdojo.Grype
 			case artifact.Semgrep:
@@ -54,9 +59,21 @@ func NewExportCmd(service DDExportService, timeout time.Duration, engagement def
 				return fmt.Errorf("%w: Unsupported file type", ErrorEncoding)
 			}
 
+			if rType != artifact.Cyclonedx && fullBom {
+				return errors.New("--full-bom is only permitted with a CycloneDx file")
+			}
+
+			if fullBom {
+				buf := bytes.NewBuffer(fileBytes)
+				c := artifact.DecodeJSON[artifact.CyclonedxSbomReport](buf)
+				fileBytes, _ = json.Marshal(c.ShimComponentsAsVulnerabilities())
+			}
+
 			return service.Export(ctx, bytes.NewBuffer(fileBytes), engagement, ddScanType)
 		},
 	}
+
+	exportCmd.PersistentFlags().BoolP("full-bom", "m", false, "CycloneDx: Adds all the components with no vulnerabilities as SeverityNone")
 	exportCmd.AddCommand(defectDojoCmd)
 	return exportCmd
 }
